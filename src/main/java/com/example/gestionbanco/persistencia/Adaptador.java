0b6f4c24dc21fdbf796efcae4bbce3dc1db9c9a0
tfermoso <tomas.fermoso@gmail.com>
2022-03-15 16:20:46 +0100

    iniciando el Adaptador

--- src/main/java/com/example/gestionbanco/persistencia/Adaptador.java
+++ src/main/java/com/example/gestionbanco/persistencia/Adaptador.java
@@ -10,10 +10,18 @@ class PersonaAdapter extends TypeAdapter<Persona> {
     @Override
     public Persona read(JsonReader reader) throws IOException {
      return new Persona("Juan",4);
+
     }
 
     @Override
-    public void write(JsonWriter writer, Persona persona) throws IOException {
+    public void write(JsonWriter jsonWriter, Persona persona) throws IOException {
+        // Comienza la serialización de flujo en objetos
+        jsonWriter.beginObject();
+        // Especifique el valor clave de Json para comenzar con letras mayúsculas
+        jsonWriter.name("Name").value(persona.getNombre());
+        jsonWriter.name("Age").value(persona.getEdad());
 
+        // Fin de la serialización de transmisión
+        jsonWriter.endObject();
     }
 }
